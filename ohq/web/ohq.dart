4bf33e9cb8ad8b764cba5b03aad9badf35be22a5
Tate Mandel <tmandel@poTATEo.(none)>
2013-09-06 22:05:54 -0400

    Added some boring stuff

---
+++ ohq/web/ohq.dart
@@ -0,0 +1,11 @@
+library ohq;
+
+import '../lib/model.dart';
+import 'package:web_ui/web_ui.dart';
+
+@observable List<int> queue = [2];
+
+void main() {
+  //queue = new List();
+  queue.add(2);
+}
